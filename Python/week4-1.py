fa0cdf85863be8311b93d9f54698a53e0f2378b5
WalaaAbdeltwwab <walaa.mohammed91@gmail.com>
2018-11-13 10:21:33 +0200

    New Update

---
+++ Python/week4-1.py
@@ -0,0 +1,7 @@
+num = float(input('Enter a number: '))
+if num % 2 == 1 :
+    print('Number is odd')
+elif num % 2 == 0 :
+    print('Number is even')
+else :
+    print('Number is decimal')
---
+++ Python/week4-2.py
@@ -0,0 +1,7 @@
+print('Numbers from 1 to 10')
+num_1 = 7
+while num_1==7:
+    num_2 = float(input('Guess the number: '))
+    if num_2 == num_1 :
+        print('Great! You did it')
+        break
